1e45c3ee8dbe164633818826d52197a0a4350c55
JeongTaekCho <cjt3591@gmail.com>
2024-10-27 17:23:01 +0900

    Design: Tab 커스텀

--- App.tsx
+++ App.tsx
@@ -5,15 +5,57 @@ import ShoppingScreen from './components/screens/ShoppingScreen';
 import {RootStackParamList, RouteNames} from './routes';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BrowserScreen from './components/screens/BrowserScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator();
 
+const HomeIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  return (
+    <MaterialCommunityIcons
+      name={focused ? 'home' : 'home-outline'}
+      color={color}
+      size={26}
+    />
+  );
+};
+
+const ShoppingIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  return (
+    <MaterialCommunityIcons
+      name={focused ? 'shopping' : 'shopping-outline'}
+      color={color}
+      size={26}
+    />
+  );
+};
+
 const HomeTab = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={RouteNames.HOME} component={HomeScreen} />
-      <Tab.Screen name={RouteNames.SHOPPING} component={ShoppingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#000',
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+      }}>
+      <Tab.Screen
+        name={RouteNames.HOME}
+        component={HomeScreen}
+        options={{
+          tabBarLabel: '홈',
+          tabBarIcon: HomeIcon,
+        }}
+      />
+      <Tab.Screen
+        name={RouteNames.SHOPPING}
+        component={ShoppingScreen}
+        options={{
+          tabBarLabel: '쇼핑',
+          tabBarIcon: ShoppingIcon,
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- components/screens/ShoppingScreen.tsx
+++ components/screens/ShoppingScreen.tsx
@@ -1,12 +1,16 @@
 import {Text, TouchableOpacity, View} from 'react-native';
 import {RootStackParamList, RouteNames} from '../../routes';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 export default function ShoppingScreen({navigation}: Props) {
   return (
     <View>
       <Text>Shopping Screen</Text>
+      <View>
+        <MaterialCommunityIcons name="shopping" size={30} color="#f00" />
+      </View>
       <TouchableOpacity
         onPress={() => {
           navigation.navigate(RouteNames.BROWSER);
